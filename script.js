129d7c287eec612940faaecec2099dc47b96ab92
Max Alterot <101337822+alterot@users.noreply.github.com>
2023-07-23 15:58:24 +0200

    Update scrolling effect

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 const closeArrows = document.querySelectorAll('.close-arrow');
+const headers = document.querySelectorAll('.heading');
 
-const headerElements = document.querySelectorAll('.heading'); // Select all header elements
-
-headerElements.forEach(header => {
+headers.forEach(header => {
     const h2 = header.querySelector('h2');
-    const content = header.querySelector('.content'); // Assuming .content is inside the .header element
+    const content = header.querySelector('.content');
 
     h2.addEventListener('click', () => {
-        // Check if the content is visible
         const isVisible = content.classList.contains('visible');
-        
-        // If the content is not visible, add the 'visible' class
+
         if (!isVisible) {
             content.classList.add('visible');
+        } else {
+            content.classList.remove('visible');
         }
 
-        // Listen for the 'transitionend' event
         const handleTransitionEnd = () => {
-            // Remove the event listener after the transition is completed
             content.removeEventListener('transitionend', handleTransitionEnd);
-
-            // Scroll to the top of the h2 element when clicked on
             h2.scrollIntoView({ behavior: 'smooth' });
         };
 
-        // Add the 'transitionend' event listener
         content.addEventListener('transitionend', handleTransitionEnd);
     });
 });
-
-
 
 closeArrows.forEach(closeArrow => {
   closeArrow.addEventListener('click', () => {
